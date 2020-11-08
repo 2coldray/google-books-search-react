87af2b111860a52abd45757ee49df5c2482142eb
Ray Bray <rbray.bray2@gmail.com>
2020-11-07 20:14:08 -0500

    input changes on value

--- client/src/containers/Search/Search.jsx
+++ client/src/containers/Search/Search.jsx
@@ -5,27 +5,29 @@ const Search = () => {
   const [bookSearch, setBookSearch] = useState("");
 
   const handleInputChange = (e) => {
-    const { name, value } = event.target;
-    setBookSearch({
-      [name]: value,
-    });
+    const { value } = e.target;
+    setBookSearch(value);
   };
 
   return (
-    <form>
-      <div class="form-group">
-        <label htmlFor="exampleFormControlInput1">Book</label>
-        <input
-          className="form-control"
-          id="exampleFormControlInput1"
-          placeholder="Search"
-          name="bookSearch"
-          value={bookSearch}
-          onChange={handleInputChange}
-        />
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div className="container">
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlInput1">Book</label>
+          <input
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="Search"
+            name="bookSearch"
+            value={bookSearch}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
