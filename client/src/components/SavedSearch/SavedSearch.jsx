a5793928504d889185b1a73e8b7d889b25d0de3d
Ray Bray <rbray.bray2@gmail.com>
2020-11-07 23:34:38 -0500

    edited save page

--- client/src/components/SavedSearch/SavedSearch.jsx
+++ client/src/components/SavedSearch/SavedSearch.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import axios from "axios";
 
-const SavedSearch = () => {
+const SavedSearch = (props) => {
+
+    const viewBook = () => {
+        let path = props.link;
+        window.open(path);
+      };
+
+    const deleteBook = (e) => {
+        const id = e.target.getAttribute("id");
+        axios.delete(`/api/books/${id}`).then((res) => {
+            console.log(res)
+            props.loadAllBooks()
+        })
+    }
+
     return (
-        <div>
-            
-        </div>
+        <div className="container text-center">
+        <li className="list-group-item">
+          <h2>{props.title}</h2>
+          <h3>By: {props.authors}</h3>
+          <p>{props.description}</p>
+          <img src={props.image} />
+          <button type="submit"  id="id" className="btn btn-danger mr-2" onClick={deleteBook}>Delete Book</button>
+          <button type="submit" className="btn btn-secondary" onClick={viewBook}>View Book</button>
+        </li>
+      </div>
     );
 };
 
--- client/src/containers/Saved/Saved.jsx
+++ client/src/containers/Saved/Saved.jsx
@@ -1,11 +1,37 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import SavedSearch from "../../components/SavedSearch/SavedSearch";
 
 const Saved = () => {
 
-  return (
+  const [getBooks, setGetBooks] = useState([]);
 
-  );
+  useEffect(() => {
+    loadAllBooks();
+  }, []);
+
+  const loadAllBooks = () => {
+    axios.get("/api/books").then((response) => {
+      setGetBooks(response.data);
+    });
+  };
+
+  return (
+    <div className="container">
+      <ul className="list-group">
+        {getBooks.map((book) => (
+          <SavedSearch 
+          title={book.title}
+          authors={book.authors}
+          description={book.description}
+          image={book.image}
+          id={book._id}
+          loadAllBooks={loadAllBooks}
+          />
+        ))}
+      </ul>
+    </div>
+  )
 };
 
 export default Saved;
