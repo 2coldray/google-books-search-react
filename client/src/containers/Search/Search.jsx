21a7a7f2ea0e01c373564e8abca540021c6c091f
Ray Bray <rbray.bray2@gmail.com>
2020-11-07 20:05:13 -0500

    made form

--- client/src/containers/Search/Search.jsx
+++ client/src/containers/Search/Search.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
 const Search = () => {
-    return (
-        <div>
-            <h3>Search Page</h3>
-        </div>
-    );
+  const [bookSearch, setBookSearch] = useState("");
+
+  const handleInputChange = (e) => {
+    const { name, value } = event.target;
+    setBookSearch({
+      [name]: value,
+    });
+  };
+
+  return (
+    <form>
+      <div class="form-group">
+        <label htmlFor="exampleFormControlInput1">Book</label>
+        <input
+          className="form-control"
+          id="exampleFormControlInput1"
+          placeholder="Search"
+          name="bookSearch"
+          value={bookSearch}
+          onChange={handleInputChange}
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  );
 };
 
 export default Search;
