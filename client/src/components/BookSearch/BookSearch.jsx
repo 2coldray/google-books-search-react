7027463f68c357215bd955eaada285f566528ce1
Ray Bray <rbray.bray2@gmail.com>
2020-11-07 22:20:53 -0500

    view button works

--- client/src/components/BookSearch/BookSearch.jsx
+++ client/src/components/BookSearch/BookSearch.jsx
@@ -17,7 +17,8 @@ const BookSearch = (props) => {
   };
 
   const viewBook = () => {
-
+    let path = book.link;
+    window.open(path);
   }
 
   return (
@@ -27,7 +28,6 @@ const BookSearch = (props) => {
         <h3>By: {props.authors}</h3>
         <p>{props.description}</p>
         <img src={props.image} />
-        <p>Buy Here: {props.link}</p>
         <button type="submit" className="btn btn-success mr-2" onClick={saveBook}>Save Book</button>
         <button type="submit" className="btn btn-secondary" onClick={viewBook}>View Book</button>
       </li>
