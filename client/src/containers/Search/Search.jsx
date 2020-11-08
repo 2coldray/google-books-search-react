cebb8664928b3f8feaca8bbcc453b1de4bd28e27
Ray Bray <rbray.bray2@gmail.com>
2020-11-07 20:49:06 -0500

    added new hook to hold the array of items

--- client/src/containers/Search/Search.jsx
+++ client/src/containers/Search/Search.jsx
@@ -1,12 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = () => {
   const [bookSearch, setBookSearch] = useState("");
+  const [bookResults, setBookResults] = useState([]);
+
+//   useEffect()
 
   const handleInputChange = (e) => {
     const { value } = e.target;
     setBookSearch(value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .get(`https://www.googleapis.com/books/v1/volumes?q=${bookSearch}`)
+      .then((res) => {
+        console.log(res);
+        setBookResults(res.data.items);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
@@ -23,10 +39,22 @@ const Search = () => {
             onChange={handleInputChange}
           />
         </div>
-        <button type="submit" className="btn btn-primary">
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={handleFormSubmit}
+        >
           Submit
         </button>
       </form>
+      <h2 className="text-center">Books</h2>
+      <div className="container">
+          <div className="row">
+              <div className="col-sm-12">
+
+              </div>
+          </div>
+      </div>
     </div>
   );
 };
