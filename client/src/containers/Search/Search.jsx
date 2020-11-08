5c627981aa15abddeb8e2c135e5609706b3ed3b9
Ray Bray <rbray.bray2@gmail.com>
2020-11-07 21:19:37 -0500

    added results component to search page

--- client/src/containers/Search/Search.jsx
+++ client/src/containers/Search/Search.jsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import BookSearch from "../../components/BookSearch/BookSearch";
 
 const Search = () => {
   const [bookSearch, setBookSearch] = useState("");
   const [bookResults, setBookResults] = useState([]);
 
-//   useEffect()
+  //   useEffect()
 
   const handleInputChange = (e) => {
     const { value } = e.target;
@@ -49,11 +50,26 @@ const Search = () => {
       </form>
       <h2 className="text-center">Books</h2>
       <div className="container">
-          <div className="row">
-              <div className="col-sm-12">
-
-              </div>
+        <div className="row">
+          <div className="col-sm-12">
+            {bookResults === undefined || bookResults.length === 0
+              ? "No books with that title"
+              : bookResults.map((res) => (
+                  <Result
+                    key={res.selfLink}
+                    title={res.volumeInfo.title}
+                    authors={res.volumeInfo.authors}
+                    description={res.volumeInfo.description}
+                    link={res.volumeInfo.infoLink}
+                    image={
+                      res.volumeInfo.imageLinks === undefined
+                        ? ""
+                        : res.volumeInfo.imageLinks.thumbnail
+                    }
+                  />
+                ))}
           </div>
+        </div>
       </div>
     </div>
   );
